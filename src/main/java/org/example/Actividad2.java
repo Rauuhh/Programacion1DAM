2c7eafd9161bf6ea7c7825463037e5df01f21ca9
A706\dam1 <raildetrenes@gmail.com>
2025-09-25 18:12:30 +0200

    Practica1

---
+++ src/main/java/org/example/Actividad2.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Actividad2 {
+    static void main() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("¿Cómo te llamas?:");
+        String nombre = entrada.nextLine();
+        System.out.println("¿A qué curso vas?:");
+        String curso = entrada.nextLine();
+        System.out.println("Hola " + nombre + ", bienvenid@ a " + curso);
+
+        System.out.println("Introduce un numero");
+        int numero1 = entrada.nextInt();
+        System.out.println("Introduce otro numero");
+        int numero2 = entrada.nextInt();
+        int result = numero1 + numero2;
+        System.out.println("El resultado de sumar los 2 numeros es: " + result);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,21 +6,10 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        //Hola
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("¿Cómo te llamas?:");
-        String nombre = entrada.nextLine();
-        System.out.println("¿A qué curso vas?:");
-        String curso = entrada.nextLine();
-        System.out.println("Hola " + nombre + ", bienvenid@ a " + curso);
 
-        System.out.println("Introduce un numero");
-        int numero1 = entrada.nextInt();
-        System.out.println("Introduce otro numero");
-        int numero2 = entrada.nextInt();
-        int result = numero1 + numero2;
-        System.out.println("El resultado de sumar los 2 numeros es: " + result);
         }
     }
 
